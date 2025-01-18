1403b4b29901c3808c77a560e8a1dcbeef4b5df8
GavinHemsada <gavinhemsada@gmail.com>
2025-01-18 20:01:50 +0530

    delete files

--- Clinic Management System/ztest.php
+++
@@ -1,8 +0,0 @@
-<?php
-include "Connection.php";
-$appointments_check = mysqli_query($con, "SELECT * FROM appointment WHERE Patient_id = 'P_EV1' and Date = '2024-09-30'and  Treatment_type = 'Eye Care'");
-if(mysqli_num_rows($appointments_check)>0){
-    echo mysqli_num_rows($appointments_check);
-}else{
-    echo "ba";
-}
